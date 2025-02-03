b4faa31fd2252c895eb43f67cb059395db2674f2
kant003 <gonzalezm.angel@gmail.com>
2025-02-03 23:47:42 +0100

    ofert

--- src/services/offer.service.ts
+++ src/services/offer.service.ts
@@ -13,7 +13,7 @@ export class OfferService {
 
      // localhost:3000/api/offer/?title=dam
      static async getAll(title: string = ''){
-        return await prisma.offer.findMany({
+       /*  return await prisma.offer.findMany({
             where: title ? {
                 title: {
                     contains: title
@@ -23,7 +23,29 @@ export class OfferService {
                 createdAt: 'desc'
             },
             take: 100
-        })
+        }) */
+
+            return await prisma.offer.findMany({
+                where: {
+                    ...(title && {
+                        title: {
+                            contains: title,
+                            //mode: "insensitive" // Búsqueda sin distinción entre mayúsculas y minúsculas
+                        }
+                    })
+                },
+                orderBy: {
+                    createdAt: 'desc'
+                },
+                take: 100,
+                include: {
+                    category: {
+                        select: {
+                            name: true
+                        }
+                    }
+                }
+            });
      }
 
      static async create(idUser: number, offer: Offer){
@@ -36,7 +58,7 @@ export class OfferService {
      }
 
      static async update(id: number, offer: Offer){
-        const findOffer = prisma.offer.findUnique({where:{id}})
+        const findOffer = await prisma.offer.findUnique({where:{id}})
         if(!findOffer) throw new HttpException(404, 'Offer doesnt exists')
         return await prisma.offer.update({
             where: {id},
@@ -46,9 +68,14 @@ export class OfferService {
         })
      }
 
-     static async delete(id: number){
-        return prisma.offer.delete({where:{id}})
-     }
+     static async delete(id: number) {
+        try {
+            return await prisma.offer.delete({ where: { id } });
+        } catch (error) {
+            throw new HttpException(404, "Offer not found");
+        }
+    }
+    
 
      
      static async rate(idUser: number, id: number, value: number){
@@ -59,16 +86,5 @@ export class OfferService {
      static async getRate(id: number){
        
      }
-
-
-
-
-    static async getByEmail(email: string){
-       const findUser = await prisma.user.findUnique(
-        { where: {email}, omit: {password:true}}
-        )
-       if(!findUser) throw new HttpException(404,'User not found')
-        return findUser
-    }
 
 }
