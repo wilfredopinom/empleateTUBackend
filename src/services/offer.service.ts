98eaf9883ebbac190488afeaa1630ae3ad881462
kant003 <gonzalezm.angel@gmail.com>
2025-02-05 11:13:59 +0100

    cambios finales

--- src/services/offer.service.ts
+++ src/services/offer.service.ts
@@ -78,18 +78,43 @@ export class OfferService {
     
 
      
-<<<<<<< HEAD
-     static async rate(id: number){
-       // si existe lo actualizo
-=======
-     static async rate(idUser: number, id: number, value: number){
-        //si existe lo actualizo
->>>>>>> 0033bf6ee40c217886bde197ec8e9314eb2ee0cc
-       // si no existe lo creo
-     }
-
-     static async getRate(id: number){
-       
-     }
+    static async rate(idOffer: number, idUser: number, value: number): Promise<void> {
+        // Validar que el rating está dentro del rango permitido
+        if (value < 0 || value > 5) {
+          throw new Error("Rating must be between 0 and 5.");
+        }
+    
+        // Verificar si la oferta existe
+        const offer = await prisma.offer.findUnique({ where: { id: idOffer } });
+        if (!offer) {
+          throw new Error("Offer not found.");
+        }
+    
+        // Actualizar o crear la calificación
+    
+        /*
+        SELECT  AVG(value) AS averageValue, COUNT(value) AS totalCount
+    FROM Rating
+    WHERE offerId = <offerId>;
+        */
+        await prisma.rate.upsert({
+          where: { idUser_idOffer: { idUser, idOffer } },
+          update: { value },
+          create: { idUser, idOffer, value },
+        });
+      }
+    
+    
+      static async getRate(idOffer: number) {
+        const ratingStats = await prisma.rate.aggregate({
+          where: { idOffer },
+          _avg: { value: true }, // Calcular el promedio
+          _count: { value: true }, // Contar el total de calificaciones
+        });
+        return {
+          totalRatings: ratingStats._count.value,
+          averageRating: ratingStats._avg.value?.toFixed(2)
+        }
+      }
 
 }
