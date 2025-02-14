a6576eb3db72c110a068b710b15dbac8a234477b
wilfredopinom <wilfredopinom@gmail.com>
2025-02-14 10:27:06 +0100

    logout

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,33 +1,42 @@
 import { AuthService } from "../services/auth.service";
-import {Response, Request, NextFunction} from 'express'
+import { Response, Request, NextFunction } from 'express'
 
-export class AuthController{
-    static async register(req:Request, res:Response, next: NextFunction){
-        try{
+export class AuthController {
+    static async register(req: Request, res: Response, next: NextFunction) {
+        try {
             const userData = req.body
             //TODO validar el body
             const newUser = await AuthService.register(userData)
-            res.status(201).json({message:'User register successfully', newUser})
-        }catch(error){
+            res.status(201).json({ message: 'User register successfully', newUser })
+        } catch (error) {
             next(error)
         }
 
     }
 
-    static async login(req:Request, res:Response, next: NextFunction){
-        try{
+    static async login(req: Request, res: Response, next: NextFunction) {
+        try {
             const userData = req.body
             const token = await AuthService.login(userData.email, userData.password)
             //TODO inyectar cookie al cliente
             res.cookie('token', token, {
-                maxAge: 60*60*1000*3, // 3 horas de caducidad
+                maxAge: 60 * 60 * 1000 * 3, // 3 horas de caducidad
                 httpOnly: true, // no se puede accerder mediante js
                 secure: false, // solo se envia si usas https
                 sameSite: 'strict', // Evita ataques CSRF
 
             })
-            res.status(201).json({message:'Login successfully:', token})
-        }catch(error){
+            res.status(201).json({ message: 'Login successfully:', token })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async logout(req: Request, res: Response, next: NextFunction) {
+        try {
+            res.clearCookie('token')
+            res.status(204).json({ message: 'Logout successfully:' })
+        } catch (error) {
             next(error)
         }
     }
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -5,7 +5,7 @@ import { ValidationMiddleware } from "../middlewares/validation.middleware";
 const router = Router()
 
 router.post('/login', loginValidation, ValidationMiddleware, AuthController.login)
-// router.post('/logout', AuthController.logout)
+router.post('/logout', AuthController.logout)
 router.post('/register', registerValidation, ValidationMiddleware, AuthController.register)
 
 export default router
